698b88f00a2550d50a9b4844f5157777258cb999
smittechiee <smit.patel@trootech.com>
2023-03-10 19:22:18 +0530

    first commit

---
+++ board_logic.js
@@ -0,0 +1,11 @@
+function Add_Item() {
+    document.getElementById("popupform").style.display="block";
+
+}
+
+function Close_Item() {
+    document.getElementById("popupform").style.display="none";
+
+}
+
+
